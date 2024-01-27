796498727f95b60154d5ed92a20b09db95e4d3b2
NamDP <ealflm@gmail.com>
2024-01-27 12:13:22 +0700

    add es linter

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -1,3 +1,4 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls = require "null-ls"
 
 local b = null_ls.builtins
@@ -5,8 +6,11 @@ local b = null_ls.builtins
 local sources = {
 
   -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  -- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
   b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  b.diagnostics.eslint,
+  b.formatting.eslint,
+  b.code_actions.eslint,
 
   -- Lua
   b.formatting.stylua,
@@ -18,4 +22,19 @@ local sources = {
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end
 }
