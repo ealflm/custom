fb632291421663f15b055af05e50d69a2beac293
NamDP <ealflm@gmail.com>
2024-03-14 18:47:26 +0700

    commit

--- options.lua
+++ options.lua
@@ -11,15 +11,16 @@ opt.autoread = true
 opt.history = 999
 opt.relativenumber = true
 opt.swapfile = false
-opt.guifont = {"JetBrainsMonoNL Nerd Font", ":h11"}
+opt.guifont = { "JetBrainsMonoNL Nerd Font", ":h11" }
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldenable = false
 
-opt.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
-opt.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
-opt.shellredir = "2>&1 | %{ \"$_\" } | Out-File %s; exit $LastExitCode"
-opt.shellpipe = "2>&1 | %{ \"$_\" } | tee %s; exit $LastExitCode"
+opt.shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell"
+opt.shellcmdflag =
+  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
+opt.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+opt.shellpipe = '2>&1 | %{ "$_" } | tee %s; exit $LastExitCode'
 opt.shellquote = ""
 opt.shellxquote = ""
 
@@ -36,9 +37,9 @@ vim.api.nvim_create_autocmd("FileType", {
     opt.shiftwidth = 2
     opt.tabstop = 2
     opt.softtabstop = 2
-  end
+  end,
 })
 
 vim.cmd "set nofixendofline"
 
-vim.api.nvim_set_keymap('', '<A-w>', '<C-w>', { noremap = true })
+vim.api.nvim_set_keymap("", "<A-w>", "<C-w>", { noremap = true })
