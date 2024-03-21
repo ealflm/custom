4fd5a156e78ebd4385a509b93d42470db2e2cc81
NamDP <ealflm@gmail.com>
2024-03-21 18:07:19 +0700

    WIP

--- configs/conform.lua
+++ configs/conform.lua
@@ -4,10 +4,11 @@ require("conform").setup {
     -- Conform will run multiple formatters sequentially
     python = { "isort", "black" },
     -- Use a sub-list to run only the first available formatter
-    javascript = { { "prettierd", "prettier" } },
-    typescript = { { "prettierd", "prettier" } },
-    typescriptreact = { { "prettierd", "prettier" } },
-    javascriptreact = { { "prettierd", "prettier" } },
+    javascript = { "prettierd", "prettier" },
+    typescript = { "prettierd", "prettier" },
+    typescriptreact = { "prettierd", "prettier" },
+    javascriptreact = { "prettierd", "prettier" },
+    json = { "fixjson", "jsonnetfmt", "yq" },
   },
   format_after_save = {
     lsp_fallback = true,
