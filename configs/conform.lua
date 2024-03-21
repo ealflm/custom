aee9b4e4f65c0b72f2065adca824b17fc8806229
NamDP <ealflm@gmail.com>
2024-03-21 11:12:35 +0700

    WIP

--- configs/conform.lua
+++ configs/conform.lua
@@ -6,6 +6,8 @@ require("conform").setup {
     -- Use a sub-list to run only the first available formatter
     javascript = { { "prettierd", "prettier" } },
     typescript = { { "prettierd", "prettier" } },
+    typescriptreact = { { "prettierd", "prettier" } },
+    javascriptreact = { { "prettierd", "prettier" } },
   },
   format_after_save = {
     lsp_fallback = true,
--- configs/lint.lua
+++ configs/lint.lua
@@ -1,8 +1,10 @@
-local lint = require("lint");
+local lint = require "lint"
 
 lint.linters_by_ft = {
   javascript = { "eslint_d" },
-  typescript = { "eslint_d" }
+  typescript = { "eslint_d" },
+  javascriptreact = { "eslint_d" },
+  typescriptreact = { "eslint_d" },
 }
 
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
@@ -11,5 +13,5 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextCh
   group = lint_augroup,
   callback = function()
     lint.try_lint()
-  end
+  end,
 })
