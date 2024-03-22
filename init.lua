44484c1eae7fb956708c555a829a5bc0003d412d
NamDP <ealflm@gmail.com>
2024-03-22 13:05:01 +0700

    WIP

--- init.lua
+++ init.lua
@@ -2,3 +2,9 @@ require "custom.options"
 require "custom.presets"
 
 vim.g.vscode_snippets_path = "C:/Users/ealflm/AppData/Roaming/Code/User/snippets"
+
+vim.keymap.set("i", "<C-l>", 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false,
+})
+vim.g.copilot_no_tab_map = true
